8e979c623955f4282203757571c9e227b5941a5f
Rias <hello@rias.be>
2018-05-01 10:30:45 +0000

    Apply fixes from StyleCI

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -26,7 +26,7 @@ class Settings extends Model
     // =========================================================================
 
     /**
-     * @var boolean
+     * @var bool
      */
     public $sync = true;
 
