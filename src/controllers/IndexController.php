265af562fd72d7c972f88c7c81370ba1c448fe19
Nic Gordon <nic.gordon@inlight.com.au>
2019-08-23 15:50:10 +1000

    Add standard doc blocks

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Scout plugin for Craft CMS 3.x.
+ *
+ * Craft Scout provides a simple solution for adding full-text search to your entries. Scout will automatically keep your search indexes in sync with your entries.
+ *
+ * @link      https://rias.be
+ *
+ * @copyright Copyright (c) 2017 Rias
+ */
+
 
 namespace rias\scout\controllers;
 
