6c6b51c48ebcee06e2659834225db75f96670462
Jan Henckens <jan@henckens.be>
2021-10-07 21:03:08 +0200

    Check if we have settings before trying to dump them

--- src/utilities/ScoutUtility.php
+++ src/utilities/ScoutUtility.php
@@ -30,6 +30,7 @@ class ScoutUtility extends Utility
 
         $engines = Scout::$plugin->getSettings()->getEngines();
 
+
         $stats = $engines->map(function (Engine $engine) {
             return [
                 'name'        => $engine->scoutIndex->indexName,
@@ -37,6 +38,7 @@ class ScoutUtility extends Utility
                 'site'        => $engine->scoutIndex->criteria->siteId === '*' ? 'all' : Craft::$app->getSites()->getSiteById($engine->scoutIndex->criteria->siteId),
                 'indexed'     => $engine->getTotalRecords(),
                 'elements'    => $engine->scoutIndex->criteria->count(),
+                'hasSettings' => $engine->scoutIndex->indexSettings ?? null
             ];
         });
 
