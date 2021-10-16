4793b2231c1cdeda4ea1e63575cb5aeafc92d831
Jan Henckens <janhenckens@users.noreply.github.com>
2021-10-16 15:34:02 +0000

    Apply fixes from StyleCI

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -5,7 +5,7 @@ return [
     'user'        => 'root',
     'database'    => 'scout_testing',
     'tablePrefix' => '',
-    'driver'      => "mysql",
+    'driver'      => 'mysql',
     'port'        => 3306,
     'schema'      => getenv('DB_SCHEMA'),
     'server'      => '127.0.0.1',
