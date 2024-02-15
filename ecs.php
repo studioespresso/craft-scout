e1bec3b6fcfe1b5540a44eb66088fae61a919f4e
Jan Henckens <jan@henckens.be>
2024-02-15 17:05:02 +0100

    Update dev deps for 8.2

---
+++ ecs.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use craft\ecs\SetList;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function(ECSConfig $ecsConfig): void {
+    $ecsConfig->parallel();
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __FILE__,
+    ]);
+
+    $ecsConfig->sets([SetList::CRAFT_CMS_4]); // for Craft 4 projects
+};
