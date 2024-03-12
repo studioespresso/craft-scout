3c11e78b3a8dc42ba0b5c29eca14cfbbbc82040b
qrazi <qrazi@sivlingworkz.com>
2024-03-12 14:43:57 +0100

    feat!: upgrade `league/fractal` to 0.20
    Upgrade to most recent branch of `league/fractal`. This makes sense to do now since `craftcms/element-api` has also done this upgrade in its recent 4.x line.
    
    BREAKING CHANGE: Fractal's public API has changed in classes such as `\League\Fractal\Serializer\ArraySerializer`

--- src/serializer/AlgoliaSerializer.php
+++ src/serializer/AlgoliaSerializer.php
@@ -7,12 +7,12 @@ class AlgoliaSerializer extends \League\Fractal\Serializer\ArraySerializer
     /**
      * Serialize a collection.
      *
-     * @param string $resourceKey
+     * @param ?string $resourceKey
      * @param array  $data
      *
      * @return array
      */
-    public function collection($resourceKey, array $data)
+    public function collection(?string $resourceKey, array $data): array
     {
         if ($resourceKey) {
             return [$resourceKey => $data];
