0a47413eee9acb4edd8fe5387731ba9aee7f553f
Jan Henckens <jan@henckens.be>
2024-03-15 12:02:48 +0100

    Readme update

--- config/scout.php
+++ config/scout.php
@@ -87,7 +87,7 @@ return [
     /**
      * The element types to look for when indexRelations is enabled.
      * By default, all Craft elements are checked for relations.
-     * Use this to avoid unnecissary queries to Elements that aren't
+     * Use this to avoid unnecessary queries to Elements that aren't
      * used by your indices or to check custom Elements that may be
      * related to your indices
      */
