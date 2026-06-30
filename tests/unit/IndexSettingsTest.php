1c07b634f50ccf1b0ff3580cb4d49c80f2a3feb9
Jan Henckens <jan@henckens.be>
2026-06-30 20:23:01 +0200

    Test for #380

--- tests/unit/IndexSettingsTest.php
+++ tests/unit/IndexSettingsTest.php
@@ -47,4 +47,48 @@ class IndexSettingsTest extends Unit
             'minWordSizefor2Typos' => 10,
         ], $indexSettings->settings);
     }
+
+    /** @test * */
+    public function it_can_set_arbitrary_settings_like_relevancy_strictness()
+    {
+        $indexSettings = IndexSettings::create()
+            ->relevancyStrictness(100);
+
+        $this->assertEquals([
+            'relevancyStrictness' => 100,
+        ], $indexSettings->settings);
+    }
+
+    /** @test * */
+    public function it_can_set_multiple_settings_at_once_with_set_settings()
+    {
+        $indexSettings = IndexSettings::create()
+            ->setSettings([
+                'relevancyStrictness' => 100,
+                'customRanking' => ['desc(cat_status.title)'],
+            ]);
+
+        $this->assertEquals([
+            'relevancyStrictness' => 100,
+            'customRanking' => ['desc(cat_status.title)'],
+        ], $indexSettings->settings);
+    }
+
+    /** @test * */
+    public function set_settings_merges_with_and_overrides_existing_settings()
+    {
+        $indexSettings = IndexSettings::create()
+            ->minWordSizefor1Typo(4)
+            ->relevancyStrictness(50)
+            ->setSettings([
+                'relevancyStrictness' => 100,
+                'customRanking' => ['desc(price)'],
+            ]);
+
+        $this->assertEquals([
+            'minWordSizefor1Typo' => 4,
+            'relevancyStrictness' => 100,
+            'customRanking' => ['desc(price)'],
+        ], $indexSettings->settings);
+    }
 }
